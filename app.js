e3d0cb37a1de0199591eb4b79f0a6685634dfd28
Yuking Quan <yuking@Yukings-MacBook-Pro.local>
2020-11-17 22:14:07 -0800

    created seeds file that allows developer to test random database through testing files.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
-const campground = require('./models/campground');
+
 
 mongoose.connect('mongodb://localhost:27017/trave-world', {
     useNewUrlParser: true,
