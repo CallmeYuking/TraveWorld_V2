e743af7bb1e9ed3bfe947c53a2109ae37373148f
Yuking Quan <yuking@Yukings-MacBook-Pro.local>
2020-11-17 23:00:05 -0800

    create show page route for display each spot(place) details on the show page.

--- app.js
+++ app.js
@@ -21,15 +21,20 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
-
+ 
 app.get('/', (req, res) => { 
     res.render('home');
 })
 
-app.get('/spots', async(req, res) => { 
+app.get('/spots', async (req, res) => { 
     const spots = await Campground.find({});
     res.render('spots/index', { spots });
 })
+
+app.get('/spots/:id', async (req, res) => {
+    const spot = await Campground.findById(req.params.id);
+    res.render('spots/show', { spot })
+} )
 
 app.listen(3000, () => {
     console.log('Server on port 3000')
