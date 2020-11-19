2f28736a65cd3e13a2dde5ba4bbc03be60806989
Yuking Quan <yuking@Yukings-MacBook-Pro.local>
2020-11-19 00:47:43 -0800

    adding image api that allows database accesses the img and display on the show page.

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 100) + 10;
         const place = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/885364',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati corporis maxime ad vitae sunt est distinctio neque, delectus mollitia autem optio libero magnam. Rem quod ratione aliquam quaerat a modi!',
+            price: price
         });
         await place.save();
     }
