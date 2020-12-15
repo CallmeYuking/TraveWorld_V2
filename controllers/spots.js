745e01cc292470a357ff56131e0136b54e74ad5b
Yuking Quan <yuking@Yukings-MacBook-Pro.local>
2020-12-14 19:03:05 -0800

    move all the functions into controller dir.

---
+++ controllers/spots.js
@@ -0,0 +1,57 @@
+const Campground = require('../models/campground');
+
+module.exports.index = async(req, res) => { 
+    const spots = await Campground.find({});
+    res.render('spots/index', { spots });
+};
+ 
+module.exports.renderNewForm = (req, res) => {
+    res.render('spots/new')
+}
+
+module.exports.createSpot = async (req, res) => {
+    // if (!req.body.spot) throw new ExpressError('Invalid Spot Data', 400);
+    const spot = new Campground(req.body.spot)
+    spot.author = req.user._id;
+    await spot.save();
+    req.flash('success', 'Successfully made a new spot!');
+    res.redirect(`/spots/${spot._id}`)
+}
+
+module.exports.showSpot = async (req, res) => {
+    const spot = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    // console.log(spot)
+    if (!spot) {
+        req.flash('error', 'Cannot find that spot!');
+        return res.redirect('/spots')
+    }
+    res.render('spots/show', { spot })
+}
+
+module.exports.showEditForm = async (req, res) => {
+    const spot = await Campground.findById(req.params.id);
+    if (!spot) {
+        req.flash('error', 'Cannot find that spot!');
+        return res.redirect('/spots')
+    }
+    res.render('spots/edit', {spot})
+}
+
+module.exports.updateEdit = async (req, res) => {
+    const { id } = req.params;
+    const spot = await Campground.findByIdAndUpdate(id, { ...req.body.spot })
+    req.flash('success', 'Successfully updated spot!');
+    res.redirect(`/spots/${spot._id}`)
+} 
+
+module.exports.deleteSpot = async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted spot!');
+    res.redirect('/spots');
+}
--- routes/spots.js
+++ routes/spots.js
@@ -1,68 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-
 const {isLoggedIn, isAuthor, validateSpot} = require('../middleware');
-
+const spots = require('../controllers/spots')
 const Campground = require('../models/campground');
 
 
 
 
-router.get('/', catchAsync(async (req, res) => { 
-    const spots = await Campground.find({});
-    res.render('spots/index', { spots });
-}))
+router.get('/', catchAsync(spots.index));
 
-router.get('/new', isLoggedIn, (req, res) => {
-    res.render('spots/new')
-})
+router.get('/new', isLoggedIn, spots.renderNewForm);
 
-router.post('/', isLoggedIn, validateSpot, catchAsync(async (req, res) => {
-    // if (!req.body.spot) throw new ExpressError('Invalid Spot Data', 400);
-    const spot = new Campground(req.body.spot)
-    spot.author = req.user._id;
-    await spot.save();
-    req.flash('success', 'Successfully made a new spot!');
-    res.redirect(`/spots/${spot._id}`)
-}))
+router.post('/', isLoggedIn, validateSpot, catchAsync(spots.createSpot));
 
-router.get('/:id', catchAsync(async (req, res) => {
-    const spot = await Campground.findById(req.params.id).populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author');
-    console.log(spot)
-    if (!spot) {
-        req.flash('error', 'Cannot find that spot!');
-        return res.redirect('/spots')
-    }
-    res.render('spots/show', { spot })
-}))
+router.get('/:id', catchAsync(spots.showSpot));
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const spot = await Campground.findById(req.params.id);
-    if (!spot) {
-        req.flash('error', 'Cannot find that spot!');
-        return res.redirect('/spots')
-    }
-    res.render('spots/edit', {spot})
-}))
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(spots.showEditForm))
 
-router.put('/:id', isLoggedIn, isAuthor, validateSpot, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const spot = await Campground.findByIdAndUpdate(id, { ...req.body.spot })
-    req.flash('success', 'Successfully updated spot!');
-    res.redirect(`/spots/${spot._id}`)
-} ))
+router.put('/:id', isLoggedIn, isAuthor, validateSpot, catchAsync(spots.updateEdit))
 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success', 'Successfully deleted spot!');
-    res.redirect('/spots');
-}))
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(spots.deleteSpot))
 
 module.exports = router;
