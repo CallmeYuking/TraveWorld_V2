ff9a8eda8ee4f561467f730121d259c085e3bad7
Yuking Quan <yuking@Yukings-MacBook-Pro.local>
2020-12-26 00:56:05 -0800

    adding map markup popup.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,11 +2,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    style: 'mapbox://styles/mapbox/dark-v10', // stylesheet location
     center: spot.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
 
 new mapboxgl.Marker()
     .setLngLat(spot.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset: 25})
+        .setHTML(
+            `<h5>${spot.title}</h5><p>${spot.location}</p>`
+        )
+    )
     .addTo(map);
