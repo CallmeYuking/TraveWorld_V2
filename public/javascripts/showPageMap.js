fa5e643a4bb792eba1fd1096041a8cc2bddbdf1d
Yuking Quan <yuking@Yukings-MacBook-Pro.local>
2020-12-26 00:26:23 -0800

    centering the map maker on a spot through passing spot.geometry.coordinates.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,12 @@
+
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9 // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: spot.geometry.coordinates, // starting position [lng, lat]
+    zoom: 9 // starting zoom
 });
+
+new mapboxgl.Marker()
+    .setLngLat(spot.geometry.coordinates)
+    .addTo(map);
